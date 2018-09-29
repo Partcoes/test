0ee82a01d6fd52eac1a56c0a7cc57cb42a6cbf99
xishiwei <1121838655@qq.com>
2018-09-29 17:24:40 +0800

    form submit

--- resources/views/home/users/register.blade.php
+++ resources/views/home/users/register.blade.php
@@ -20,8 +20,8 @@
 				</div>
 				<div class="regist_main center">
 					<div class="username">用&nbsp;&nbsp;户&nbsp;&nbsp;名:&nbsp;&nbsp;<input onblur="checkName()" class="shurukuang" type="text" id="user_name" name="user_name" placeholder="请输入邮箱或者手机号"/><span id="checkName">请输入邮箱或者手机号</span></div>
-					<div class="username">密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码:&nbsp;&nbsp;<input class="shurukuang" type="password" name="user_pwd" placeholder="请输入你的密码"/><span id="checkPwd">请输入6位以上字符</span></div>
-					<div class="username">确认密码:&nbsp;&nbsp;<input class="shurukuang" type="password" name="user_repwd" placeholder="请确认你的密码"/><span id="checkRepwd">两次密码要输入一致哦</span></div>
+					<div class="username">密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码:&nbsp;&nbsp;<input onblur="checkPwd()" id="user_pwd" class="shurukuang" type="password" name="user_pwd" placeholder="请输入你的密码"/><span id="checkPwd">请输入6位以上字符</span></div>
+					<div class="username">确认密码:&nbsp;&nbsp;<input onblur="checkRepwd()" class="shurukuang" type="password" id="user_repwd" name="user_repwd" placeholder="请确认你的密码"/><span id="checkRepwd">两次密码要输入一致哦</span></div>
 					<div class="username">
 						<div class="left fl">验&nbsp;&nbsp;证&nbsp;&nbsp;码:&nbsp;&nbsp;<input class="yanzhengma" type="text" name="verification" placeholder="请输入验证码"/></div>
 						<div class="right fl"><img src="/home/image/yanzhengma.jpg"></div>
@@ -32,17 +32,20 @@
 					<input class="submit" type="submit" value="立即注册" >
 				</div>
 				<script>
+					/** 
+						判断用户注册方式 ， 并通过ajax判断邮箱或者手机号码是否已经存在
+					**/
 					function checkName()
 					{
 						var user_name = $('#user_name').val();
 						var regEmail = /^[A-Za-z0-9]+\@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
 						var regTel = /^[\d]{11}$/;
 						if (regEmail.test(user_name)) {
-							$('#checkName').text('用户邮箱登录');$('#user_name').css('border-color','green');
+							$('#checkName').text('用户邮箱登录');
 						} else if (regTel.test(user_name)) {
-							$('#checkName').text('用户手机号码登录');$('#user_name').css('border-color','green');
+							$('#checkName').text('用户手机号码登录');
 						} else {
-							$('#checkName').text('验证失败');$('#user_name').css('border-color','red');
+							$('#checkName').text('验证失败');$('#user_name').css('border-color','red');return false;
 						}
 						$.ajax({
 							type : 'post',
@@ -51,13 +54,56 @@
 							data : {'_token':'{{csrf_token()}}','userName':user_name},
 							success:function (msg) {
 								if (msg == 'null') {
-									// $('#user_name').css('border-color','green');
+									$('#user_name').css('border-color','green');return true;
 								} else {
 									alert('用户已经存在');
+									$('#user_name').css('border-color','red');return false;
 								}
 							}
 						});
 					}
+
+
+					/**@argument
+						验证密码是否符合规则
+					**/
+					function checkPwd()
+					{
+						var user_pwd = $('#user_pwd').val();
+						console.log(user_pwd);
+						var regPwd = /^[a-zA-Z\d_\.\/]{8,}$/;
+						if (regPwd.test(user_pwd)) {
+							$('#checkPwd').text('密码验证成功');$('#user_pwd').css('border-color','green');return true;
+						} else {
+							$('#checkPwd').text('密码验证失败');$('#user_pwd').css('border-color','red');return false;
+						}
+					}
+
+					/**@argument
+						确认密码
+					**/
+					function checkRepwd()
+					{
+						var user_pwd = $('#user_pwd').val();
+						var user_repwd = $('#user_repwd').val();
+						var regPwd = /^[a-zA-Z\d_\.\/]{8,}$/;
+						if (regPwd.test(user_repwd) && user_repwd == user_pwd) {
+							$('#checkRepwd').text('验证成功');$('#user_repwd').css('border-color','green');return true;
+						} else {
+							$('#checkRepwd').text('验证失败');$('#user_repwd').css('border-color','red');return false;
+						}
+					}
+
+					/**@argument
+						限制表单提交
+					**/
+					$('form').submit(function(){
+						if (checkName() && checkPwd() && checkRepwd()) {
+							return true;
+						} else {
+							return false;
+						}
+					});
 				</script>
 			</div>
 		</div>
