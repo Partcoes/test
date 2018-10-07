507aaf9375864d6587cd02e4d38de50b00e0390f
xishiwei <1121838655@qq.com>
2018-10-07 16:53:59 +0800

    Update resources/views/home/index/index.blade.php

--- resources/views/home/index/index.blade.php
+++ resources/views/home/index/index.blade.php
@@ -12,7 +12,7 @@
 								<div>
 									<div class="xuangou_left fl">
 										<a href="">
-											<div class="img fl"><img src="/home/image/xm6_80.png" alt=""></div>
+											<div class="img fl"><img src="{{URL::asset('/home/image/xm6_80.png')}}" alt=""></div>
 											<span class="fl">小米6</span>
 											<div class="clear"></div>
 										</a>
@@ -31,17 +31,17 @@
 
 		<div class="sub_banner center">
 			<div class="sidebar fl">
-				<div class="fl"><a href=""><img src="/home/image/hjh_01.gif"></a></div>
-				<div class="fl"><a href=""><img src="/home/image/hjh_02.gif"></a></div>
-				<div class="fl"><a href=""><img src="/home/image/hjh_03.gif"></a></div>
-				<div class="fl"><a href=""><img src="/home/image/hjh_04.gif"></a></div>
-				<div class="fl"><a href=""><img src="/home/image/hjh_05.gif"></a></div>
-				<div class="fl"><a href=""><img src="/home/image/hjh_06.gif"></a></div>
+				<div class="fl"><a href=""><img src="{{URL::asset('/home/image/hjh_01.gif')}}"></a></div>
+				<div class="fl"><a href=""><img src="{{URL::asset('/home/image/hjh_02.gif')}}"></a></div>
+				<div class="fl"><a href=""><img src="{{URL::asset('/home/image/hjh_03.gif')}}"></a></div>
+				<div class="fl"><a href=""><img src="{{URL::asset('/home/image/hjh_04.gif')}}"></a></div>
+				<div class="fl"><a href=""><img src="{{URL::asset('/home/image/hjh_05.gif')}}"></a></div>
+				<div class="fl"><a href=""><img src="{{URL::asset('/home/image/hjh_06.gif')}}"></a></div>
 				<div class="clear"></div>
 			</div>
-			<div class="datu fl"><a href=""><img src="/home/image/hongmi4x.png" alt=""></a></div>
-			<div class="datu fl"><a href=""><img src="/home/image/xiaomi5.jpg" alt=""></a></div>
-			<div class="datu fr"><a href=""><img src="/home/image/pinghengche.jpg" alt=""></a></div>
+			<div class="datu fl"><a href=""><img src="{{URL::asset('/home/image/hongmi4x.png')}}" alt=""></a></div>
+			<div class="datu fl"><a href=""><img src="{{URL::asset('/home/image/xiaomi5.jpg')}}" alt=""></a></div>
+			<div class="datu fr"><a href=""><img src="{{URL::asset('/home/image/pinghengche.jpg')}}" alt=""></a></div>
 			<div class="clear"></div>
 
 
@@ -54,31 +54,31 @@
 			<div class="biaoti center">小米明星单品</div>
 			<div class="main center">
 				<div class="mingxing fl">
-					<div class="sub_mingxing"><a href=""><img src="/home/image/pinpai1.png" alt=""></a></div>
+					<div class="sub_mingxing"><a href=""><img src="{{URL::asset('/home/image/pinpai1.png')}}" alt=""></a></div>
 					<div class="pinpai"><a href="">小米MIX</a></div>
 					<div class="youhui">5月9日-21日享花呗12期分期免息</div>
 					<div class="jiage">3499元起</div>
 				</div>
 				<div class="mingxing fl">
-					<div class="sub_mingxing"><a href=""><img src="/home/image/pinpai2.png" alt=""></a></div>
+					<div class="sub_mingxing"><a href=""><img src="{{URL::asset('/home/image/pinpai2.png')}}" alt=""></a></div>
 					<div class="pinpai"><a href="">小米5s</a></div>
 					<div class="youhui">5月9日-10日，下单立减200元</div>
 					<div class="jiage">1999元</div>
 				</div>
 				<div class="mingxing fl">
-					<div class="sub_mingxing"><a href=""><img src="/home/image/pinpai3.png" alt=""></a></div>
+					<div class="sub_mingxing"><a href=""><img src="{{URL::asset('/home/image/pinpai3.png')}}" alt=""></a></div>
 					<div class="pinpai"><a href="">小米手机5 64GB</a></div>
 					<div class="youhui">5月9日-10日，下单立减100元</div>
 					<div class="jiage">1799元</div>
 				</div>
 				<div class="mingxing fl"> 	
-					<div class="sub_mingxing"><a href=""><img src="/home/image/pinpai4.png" alt=""></a></div>
+					<div class="sub_mingxing"><a href=""><img src="{{URL::asset('/home/image/pinpai4.png')}}" alt=""></a></div>
 					<div class="pinpai"><a href="">小米电视3s 55英寸</a></div>
 					<div class="youhui">5月9日，下单立减200元</div>
 					<div class="jiage">3999元</div>
 				</div>
 				<div class="mingxing fl">
-					<div class="sub_mingxing"><a href=""><img src="/home/image/pinpai5.png" alt=""></a></div>
+					<div class="sub_mingxing"><a href=""><img src="{{URL::asset('/home/image/pinpai5.png')}}" alt=""></a></div>
 					<div class="pinpai"><a href="">小米笔记本</a></div>
 					<div class="youhui">更轻更薄，像杂志一样随身携带</div>
 					<div class="jiage">3599元起</div>
@@ -90,11 +90,11 @@
 			<div class="biaoti center">配件</div>
 			<div class="main center">
 				<div class="content">
-					<div class="remen fl"><a href=""><img src="/home/image/peijian1.jpg"></a>
+					<div class="remen fl"><a href=""><img src="{{URL::asset('/home/image/peijian1.jpg')}}"></a>
 					</div>
 					<div class="remen fl">
 						<div class="xinpin"><span>新品</span></div>
-						<div class="tu"><a href=""><img src="/home/image/peijian2.jpg"></a></div>
+						<div class="tu"><a href=""><img src="{{URL::asset('/home/image/peijian2.jpg')}}"></a></div>
 						<div class="miaoshu"><a href="">小米6 硅胶保护套</a></div>
 						<div class="jiage">49元</div>
 						<div class="pingjia">372人评价</div>
@@ -107,14 +107,14 @@
 					</div>
 					<div class="remen fl">
 						<div class="xinpin"><span style="background:#fff"></span></div>
-						<div class="tu"><a href=""><img src="/home/image/peijian3.jpg"></a></div>
+						<div class="tu"><a href=""><img src="{{URL::asset('/home/image/peijian3.jpg')}}"></a></div>
 						<div class="miaoshu"><a href="">小米手机4c 小米4c 智能</a></div>
 						<div class="jiage">29元</div>
 						<div class="pingjia">372人评价</div>
 					</div>
 					<div class="remen fl">
 						<div class="xinpin"><span style="background:red">享6折</span></div>
-						<div class="tu"><a href=""><img src="/home/image/peijian4.jpg"></a></div>
+						<div class="tu"><a href=""><img src="{{URL::asset('/home/image/peijian4.jpg')}}"></a></div>
 						<div class="miaoshu"><a href="">红米NOTE 4X 红米note4X</a></div>
 						<div class="jiage">19元</div>
 						<div class="pingjia">372人评价</div>
@@ -127,7 +127,7 @@
 					</div>
 					<div class="remen fl">
 						<div class="xinpin"><span style="background:#fff"></span></div>
-						<div class="tu"><a href=""><img src="/home/image/peijian5.jpg"></a></div>
+						<div class="tu"><a href=""><img src="{{URL::asset('/home/image/peijian5.jpg')}}"></a></div>
 						<div class="miaoshu"><a href="">小米支架式自拍杆</a></div>
 						<div class="jiage">89元</div>
 						<div class="pingjia">372人评价</div>
@@ -141,11 +141,11 @@
 					<div class="clear"></div>
 				</div>
 				<div class="content">
-					<div class="remen fl"><a href=""><img src="/home/image/peijian6.png"></a>
+					<div class="remen fl"><a href=""><img src="{{URL::asset('/home/image/peijian6.png')}}"></a>
 					</div>
 					<div class="remen fl">
 						<div class="xinpin"><span style="background:#fff"></span></div>
-						<div class="tu"><a href=""><img src="/home/image/peijian7.jpg"></a></div>
+						<div class="tu"><a href=""><img src="{{URL::asset('/home/image/peijian7.jpg')}}"></a></div>
 						<div class="miaoshu"><a href="">小米指环支架</a></div>
 						<div class="jiage">19元</div>
 						<div class="pingjia">372人评价</div>
@@ -158,21 +158,21 @@
 					</div>
 					<div class="remen fl">
 						<div class="xinpin"><span style="background:#fff"></span></div>
-						<div class="tu"><a href=""><img src="/home/image/peijian8.jpg"></a></div>
+						<div class="tu"><a href=""><img src="{{URL::asset('/home/image/peijian8.jpg')}}"></a></div>
 						<div class="miaoshu"><a href="">米家随身风扇</a></div>
 						<div class="jiage">19.9元</div>
 						<div class="pingjia">372人评价</div>
 					</div>
 					<div class="remen fl">
 						<div class="xinpin"><span style="background:#fff"></span></div>
-						<div class="tu"><a href=""><img src="/home/image/peijian9.jpg"></a></div>
+						<div class="tu"><a href=""><img src="{{URL::asset('/home/image/peijian9.jpg')}}"></a></div>
 						<div class="miaoshu"><a href="">红米4X 高透软胶保护套</a></div>
 						<div class="jiage">59元</div>
 						<div class="pingjia">775人评价</div>
 					</div>
 					<div class="remenlast fr">
-						<div class="hongmi"><a href=""><img src="/home/image/hongmin4.png" alt=""></a></div>
-						<div class="liulangengduo"><a href=""><img src="/home/image/liulangengduo.png" alt=""></a></div>					
+						<div class="hongmi"><a href=""><img src="{{URL::asset('/home/image/hongmin4.png')}}" alt=""></a></div>
+						<div class="liulangengduo"><a href=""><img src="{{URL::asset('/home/image/liulangengduo.png" alt=""></a></div>					
 					</div>
 					<div class="clear"></div>
 				</div>				
