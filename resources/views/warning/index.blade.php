028f01ed4e240cdc8b50a6b2e9dd7f778c73a586
xishiwei <1121838655@qq.com>
2018-10-12 13:18:31 +0800

    no message

--- resources/views/warning/index.blade.php
+++ resources/views/warning/index.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Warning</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{URL::asset('/css/bootstrap.min.css')}}">
     <script src="{{URL::asset('/js/jquery-3.1.1.min.js')}}"></script>
 </head>
 <body>
