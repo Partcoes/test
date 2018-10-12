d44f2f3efc6cb1f8c34cd192d953ebe542ea1fbe
xishiwei <1121838655@qq.com>
2018-10-12 14:42:53 +0800

    首页展示

--- app/Model/Brand.php
+++ app/Model/Brand.php
@@ -10,4 +10,12 @@ class Brand extends Model
     //定义变量
     protected $tableName = 'brands';
     protected $primaryKey = 'brand_id';
+
+    /**
+     * 关联goods表
+     */
+    public function good()
+    {
+        $this->hasOne('App\Model\Good');
+    }
 }
